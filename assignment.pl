be516e42075a265f044814b3cf3e4d3fb1d798de
Steven Noonan <stevennoonan013@gmail.com>
2017-11-15 17:36:55 +0000

    Assignment complete

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,19 @@
 %Definitions for isElementInList(El, List)
-
-%Definitions for reverseList(List, ReversedList)
-
-%insertElementIntoListEnd(El, List, NewList)
+%is the element the head of the list:
+isElementInList(Element, [Element|_]).
+%Is the element in the tail of the list:
+isElementInList(Element, [_|Tail]):- isElementInList(Element, Tail).
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([], LIST1, LIST1).
+mergeLists([HEAD | TAIL], LIST1, [HEAD | Merged]) :-
+    mergeLists(TAIL, LIST1, Merged).
+
+%Definitions for reverseList(List, ReversedList)
+reverseList([],[]).
+reverseList([HEAD | TAIL], ReversedList):-
+    mergeLists(List, [HEAD], ReversedList), reverseList(TAIL, List).
+
+
+%insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(El, List, NewList) :- mergeLists(List, [El], NewList).
